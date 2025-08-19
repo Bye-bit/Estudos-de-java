cd9f0d4e5685832c1eed795c420867b4c1abe554
Lucas Fernandes <lucasfernandes.sqn@gmail.com>
2025-08-19 19:27:19 -0300

    forgot to add these to the commit LoL

---
+++ src/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("radical");
+        Scanner scanner = new Scanner(System.in);
+        int A;
+        A = scanner.nextInt();
+        System.out.println(A);
+        scanner.close();
+    }
+}
