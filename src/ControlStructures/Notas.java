791fb3f39c448312a533c8722952af1e04cbdb2f
Lucas Fernandes <lucasfernandes.sqn@gmail.com>
2025-08-20 13:19:57 -0300

    Added more notes, and practiced a bit of control structures

---
+++ src/ControlStructures/Notas.java
@@ -0,0 +1,21 @@
+package ControlStructures;
+import java.util.Scanner;
+
+public class Notas {
+
+    public static void main(String[] args) {
+        float nota1;
+        float nota2;
+        Scanner scanner = new Scanner(System.in);
+        nota1 = scanner.nextFloat();
+        nota2 = scanner.nextFloat();
+        float media = (nota1+nota2) / 2;
+        if(media < 7) {
+            System.out.println("Reprovou!");
+        } else if(media == 7) {
+            System.out.println("Passou por com 7!");
+        } else {
+            System.out.println("Passou!");
+        }
+    }
+}
